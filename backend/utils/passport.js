c475bf928e9aa98f06fdcf5728b21da9d9087a7f
AmikaMehta123 <amika.mehta@sjsu.edu>
2022-04-24 18:52:11 -0700

    Please Work

--- backend/utils/passport.js
+++ backend/utils/passport.js
@@ -12,7 +12,7 @@ const jwt = require('jsonwebtoken');
 
 var MongoClient = require('mongodb').MongoClient;
 // var url = "mongodb://localhost:27017/";
-// var url = "mongodb+srv://AmikaMehta:AmikaMehta@cluster0.busbs.mongodb.net/etsy-database?retryWrites=true&w=majority";
+var url = "mongodb+srv://AmikaMehta:AmikaMehta@cluster0.busbs.mongodb.net/etsy-database?retryWrites=true&w=majority";
 
    
 // MongoClient.connect(url).then(function(db) {
