5f8c73d1da6daeff80de774fcccdedf0d9181230
AmikaMehta123 <amika.mehta@sjsu.edu>
2022-04-24 17:28:57 -0700

    Please Work

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 var mongoUtil = require( '../utils/mongoUtil' );
 
 
-router.post('/', function(req,res){
+router.post('/', async function(req,res){
 
     const name = req.body.name;
     const password = req.body.password;
@@ -16,40 +16,40 @@ router.post('/', function(req,res){
     // MongoClient.connect(url).then(function(db) {
     
   //if (err) throw err;
-  var dbo = mongoUtil.getDb();
+  var dbo = await mongoUtil.connectToServer();
   console.log(dbo)
-  dbo.collection("login_table").find({name,password}).toArray(function(err, result) {
+  // dbo.collection("login_table").find({name,password}).toArray(function(err, result) {
 
-    if (err) 
-    {
-        console.log("invalid credentials");
-      //  res.end("invlid credentials");
-        throw err;
-    }
-    else if (result.length == 0 || password != result[0].password) 
-        {
-            console.log("invalid credentials");
-            res.status(401)
-          //  res.end("invlid credentials");
-        }
-    else
-    {
-        if (result) {
-            const payload = { _id: result[0]._id, name: result[0].name};
-            const token = jwt.sign(payload, "cmpe273_2022", {
-            });
-            res.status(200).end("JWT " + token);
+  //   if (err) 
+  //   {
+  //       console.log("invalid credentials");
+  //     //  res.end("invlid credentials");
+  //       throw err;
+  //   }
+  //   else if (result.length == 0 || password != result[0].password) 
+  //       {
+  //           console.log("invalid credentials");
+  //           res.status(401)
+  //         //  res.end("invlid credentials");
+  //       }
+  //   else
+  //   {
+  //       if (result) {
+  //           const payload = { _id: result[0]._id, name: result[0].name};
+  //           const token = jwt.sign(payload, "cmpe273_2022", {
+  //           });
+  //           res.status(200).end("JWT " + token);
             
-        }
+  //       }
         
-        // // console.log(result);    
-        // console.log("succesfully logged in");
-        // res.status(200);
-        // res.send(name);
-    } 
-    console.log(result);
-    db.close();
-  });
+  //       // // console.log(result);    
+  //       // console.log("succesfully logged in");
+  //       // res.status(200);
+  //       // res.send(name);
+  //   } 
+  //   console.log(result);
+  //   db.close();
+  // });
 // });
 });
 
